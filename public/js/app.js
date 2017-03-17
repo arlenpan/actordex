cb742480e8bbd80153309aa2d85ba5d4291aaa42
Arlen Pan <arlenpan@gmail.com>
2017-03-16 19:23:24 -0700

    minify and deploy

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,22 @@
+// Firebase configuration
+var config = {
+  apiKey: "AIzaSyCCUaH1bW1oB7kgPUkIqRLI2-RCPyODvOo",
+  authDomain: "actor-dex-final.firebaseapp.com",
+  databaseURL: "https://actor-dex-final.firebaseio.com",
+  storageBucket: "actor-dex-final.appspot.com",
+  messagingSenderId: "810520072391"
+};
+
+// Initialize FirebaseUI
+var uiConfig = {
+  signInSuccessUrl: '/',
+  signInOptions: [
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID
+    ],
+  tosUrl: '<your-tos-url>'
+};
+
 // Initialize Firebase Database and Storage
 firebase.initializeApp(config);
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
